55f5ad49293f8599ccb1aba6aaabad11f12b85b2
Debnath <Debnath@192.168.0.2>
2018-12-11 00:51:44 +0530

    .

--- BasicPrograms/src/com/programs/strings/StringReverse.java
+++ BasicPrograms/src/com/programs/strings/StringReverse.java
@@ -2,22 +2,31 @@ package com.programs.strings;
 
 import java.util.*;
 
-public class StringReverse
-{
-   public static void main(String args[])
-   {
-      String original, reverse = "";
-      Scanner in = new Scanner(System.in);
- 
-      System.out.println("Enter a string to reverse");
-      original = in.nextLine();
- 
-      int length = original.length();
- 
-      for ( int i = length - 1 ; i >= 0 ; i-- )
-         reverse = reverse + original.charAt(i);
- 
-      System.out.println("Reverse of entered string is: "+reverse);
-      in.close();
-   }
+public class StringReverse {
+	public static void main(String args[]) {
+		String original, reverse = "";
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter a string to reverse");
+		original = in.nextLine();
+
+		int length = original.length();
+
+		for (int i = length - 1; i >= 0; i--)
+			reverse = reverse + original.charAt(i);
+
+		System.out.println("Reverse of entered string is: " + reverse);
+		in.close();
+	}
+
+	public static String reverseRecursively(String str) {
+
+		// base case to handle one char string and empty string
+		if (str.length() < 2) {
+			return str;
+		}
+
+		return reverseRecursively(str.substring(1)) + str.charAt(0);
+
+	}
 }
