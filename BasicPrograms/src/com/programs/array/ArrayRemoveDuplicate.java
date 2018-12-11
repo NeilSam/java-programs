b77a9bc7e238d4bb49e2b33e8ea707d48805e07f
Debnath Roy <Debnath@Debnaths-MacBook-Air.local>
2018-12-11 19:33:40 +0530

    .

--- BasicPrograms/src/com/programs/array/ArrayRemoveDuplicate.java
+++ BasicPrograms/src/com/programs/array/ArrayRemoveDuplicate.java
@@ -44,5 +44,14 @@ public class ArrayRemoveDuplicate {
         return result;
 
     }
+    
+	public static String removeRecursive(String word, char ch) {
+		int index = word.indexOf(ch);
+		if (index == -1) {
+			return word;
+		}
+		return removeRecursive(word.substring(0, index) + word.substring(index + 1, word.length()), ch);
+	}
+
 }
 
