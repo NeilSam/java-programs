007c0c827f45865314b8be6aac360048b2653507
Debnath Roy <debnath.roy@amadeus.com>
2018-12-11 16:11:24 +0530

    .

--- BasicPrograms/src/com/programs/strings/StringReverse.java
+++ BasicPrograms/src/com/programs/strings/StringReverse.java
@@ -29,4 +29,20 @@ public class StringReverse {
 		return reverseRecursively(str.substring(1)) + str.charAt(0);
 
 	}
+	
+    /*
+     * Java method to replace a String in place
+     */
+    public static String inPlaceReverse(final String input) {
+        final StringBuilder builder = new StringBuilder(input);
+        int length = builder.length();
+        for (int i = 0; i < length / 2; i++) {
+            final char current = builder.charAt(i);
+            final int otherEnd = length - i - 1;
+            builder.setCharAt(i, builder.charAt(otherEnd)); // swap
+            builder.setCharAt(otherEnd, current);
+        }
+
+        return builder.toString();
+    }
 }
--- BasicPrograms/src/com/programs/strings/StringRotationCheck.java
+++ BasicPrograms/src/com/programs/strings/StringRotationCheck.java
@@ -24,4 +24,30 @@ public class StringRotationCheck {
 			}
 		}
 	}
+	
+	 public static boolean isRotated(String input, String rotated) {
+
+	        if (input == null || rotated == null) {
+	            return false;
+
+	        } else if (input.length() != rotated.length()) {
+	            return false;
+
+	        }
+
+	        int index = rotated.indexOf(input.charAt(0));
+	        if (index > -1) {
+
+	            if (input.equalsIgnoreCase(rotated)) {
+	                return true;
+	            }
+
+	            int finalPos = rotated.length() - index;
+	            return rotated.charAt(0) == input.charAt(finalPos)
+	                    && input.substring(finalPos).equals(
+	                            rotated.substring(0, index));
+	        }
+	        return false;
+
+	    }
 }
