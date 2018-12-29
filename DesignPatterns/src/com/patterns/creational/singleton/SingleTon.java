637b7c8e0427322760724290c1c2065b86f0bb25
Debnath <Debnath@192.168.0.3>
2018-12-29 13:35:28 +0530

    .

--- DesignPatterns/src/com/patterns/creational/singleton/SingleTon.java
+++ DesignPatterns/src/com/patterns/creational/singleton/SingleTon.java
@@ -1,12 +1,17 @@
 package com.patterns.creational.singleton;
 
 /*
- * This pattern involves a single class which is responsible to create an object while making sure that only single object gets created. This class provides a way to access its only object which can be accessed directly without need to instantiate the object of the class.
+ * This pattern involves a single class which is responsible to create an object 
+ * while making sure that only single object gets created. This class provides a 
+ * way to access its only object which can be accessed directly without need to 
+ * instantiate the object of the class.
 
 Implementation
-We're going to create a SingleObject class. SingleObject class have its constructor as private and have a static instance of itself.
+We're going to create a SingleObject class. SingleObject class have its constructor
+ as private and have a static instance of itself.
 
-SingleObject class provides a static method to get its static instance to outside world. SingletonPatternDemo, our demo class will use SingleObject class to get a SingleObject object.
+SingleObject class provides a static method to get its static instance to outside world.
+ SingletonPatternDemo, our demo class will use SingleObject class to get a SingleObject object.
  * 
  * */
 class Singleton {
